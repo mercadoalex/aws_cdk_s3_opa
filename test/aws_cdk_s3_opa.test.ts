76de66dca529e7c734ba5334557bc2687ce4f28f
mercadoalex <mercadoalex@gmail.com>
2025-06-22 11:05:03 -0600

    Initial commit

---
+++ test/aws_cdk_s3_opa.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkS3Opa from '../lib/aws_cdk_s3_opa-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws_cdk_s3_opa-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkS3Opa.AwsCdkS3OpaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
